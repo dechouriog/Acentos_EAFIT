e5ebdefcb28a9e47324d2d2bc2752a8b6a19bae1
jespanap <espanajaider@gmail.com>
2025-10-17 10:48:48 -0500

    feature: Pusheando el repositorio

---
+++ controllers/home_controller.py
@@ -0,0 +1,17 @@
+from fastapi import Request
+from sqlalchemy.orm import Session
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from models.books import Book
+
+templates = Jinja2Templates(directory="views")
+
+def get_home(request: Request, db: Session):
+    libros = db.query(Book).limit(5).all()
+    return templates.TemplateResponse(
+        "home.html",
+        {"request": request, "libros": libros}
+    )
+
+# Para que la ruta sepa qué tipo de respuesta usa
+get_home.response_class = HTMLResponse
